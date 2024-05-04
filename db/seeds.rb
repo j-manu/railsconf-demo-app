56b05ca0d5614194a94d2085e2c7cc8538e32b34
Manu J <manu.janardhanan@lodgistics.com>
2024-05-04 16:04:53 -0400

    Add authenticaton and user
    Largely unmodified from https://github.com/joemasilotti/daily-log/commit/c807a1281319c568d5629d6a719e431dad4cd179

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.find_or_create_by!(email: 'lisan-al-gaib@arrakis.com') do |user|
+  user.name = 'Lisan Al-Gaib'
+  user.password = 'password'
+end
